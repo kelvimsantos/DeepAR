7506c0cada4f096e2eef918908b0dbfefe148d19
kelvim santos <contatoshake@hotmail.com>
2026-03-11 18:31:58 -0300

    commit game funcional ar 2

--- src/components/ARScene.jsx
+++ src/components/ARScene.jsx
@@ -16,7 +16,7 @@ const ARScene = () => {
   return (
     <>
       {/* Grupo posicionado a 2 metros na frente da câmera */}
-      <group ref={worldGroupRef} position={[0, -0.5, -6] }  >
+      <group ref={worldGroupRef} position={[0, -1, -8] }  >
         <Player />
         <World />
         <MovementController />
