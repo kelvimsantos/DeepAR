d1fcafbff2838a755648b5282e4a829c3aaac2b7
kelvim santos <contatoshake@hotmail.com>
2026-03-18 14:31:30 -0300

    incluind terreno e scene colorido funcional 5

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
   plugins: [react(), mkcert()],
+  base: '/', // ← adicione esta linha
   server: {
     host: true,
     https: true,
@@ -14,9 +15,8 @@ export default defineConfig({
       'three',
       '@react-three/fiber',
       '@react-three/drei',
-      '@react-three/rapier'   // <-- adicionado
+      '@react-three/rapier'
     ],
     exclude: ['@react-three/xr'],
   },
 });
-
